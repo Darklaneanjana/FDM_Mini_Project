7cc7bec06cba56ae651460fcaf47160817175593
NTharuka <83983046+NTharuka@users.noreply.github.com>
2022-10-09 14:01:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ st.write("This is a streamlit app")
 #print the version of streamlit
 st.write(st.__version__)
 
-#print something funny
+#print something very funny
 st.write("Developed by: Nadeesh Naluwa")
