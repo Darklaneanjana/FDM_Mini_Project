8e177bc5b4de0a3865e2cd1e217237ab471261b2
Darklaneanjana <darklaneanjana@gmail.com>
2022-10-31 15:17:48 +0530

    added menu bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
-from cgitb import small
 import streamlit as st
-#chage width in streamlit app
-
-
+from streamlit_option_menu import option_menu
+# import streamlit.components.v1 as components
 
 import numpy as np
 import pandas as pd
@@ -25,7 +23,7 @@ scalerUser=load('data/scalerUser.bin')
 scalerItem=load('data/scalerItem.bin')
 scalerTarget=load('data/scalerTarget.bin')
 
-
+# funciton to display the movie row for new users
 def newUserPredict(user_vec):
        # generate and replicate the user vector to match the number movies in the data set.
        user_train1 = pd.DataFrame(user_vec, index=[0])
@@ -50,7 +48,7 @@ def newUserPredict(user_vec):
 
        return(sorted_items.iloc[:10])
 
-
+# function to display the movie row for existing users
 def existingUserPredict(uid):
        user_train2 = pd.read_csv("data/userVector.csv").astype('float64')
        user_train2 = user_train2.loc[user_train2['userId'] == uid]
@@ -94,45 +92,66 @@ def existingUserPredict(uid):
 
        return(sorted_items.iloc[:10])
 
+st.write('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
+
+
+with st.sidebar:
+       selected = option_menu(
+              menu_title='Select the user type',
+              options = ['New User', 'Existing Users'],
+              default_index=0,
+              icons=['person', 'people'],
+              menu_icon='bars',
+       )
+       st.text('paka')
 
 
 
 
 st.write('# Content Based Movie Recommender System')
 st.write('<br>' , unsafe_allow_html=True)
-st.write('## Predictions for a new user')
-options = st.multiselect('Select Your Favourite ',categories,['Action', 'Adventure'])
-# st.write('You selected:', options)
 
-newButton = st.button('Predict_for_new_user')
-user_vec = {'userId':0, 'userRatingCount':0, 'userAvgRating':0, 'Action':0, 'Adventure':0,
-              'Animation':0, 'Children':0, 'Comedy':0, 'Crime':0, 'Documentary':0, 'Drama':0,
-              'Fantasy':0, 'Film-Noir':0, 'Horror':0, 'IMAX':0, 'Musical':0, 'Mystery':0,
-              'Romance':0, 'Sci-Fi':0, 'Thriller':0, 'War':0, 'Western':0}
-htp5= 'https://img.omdbapi.com/?apikey=a50d9a01&i=tt'
 
-if newButton:
-       with st.spinner('Wait for it...'):
-              for i in options:
-                     user_vec[i] = 5
-              moviesn = newUserPredict(user_vec)
-              # st.write(moviesn)
-              
-              for i in range(len(moviesn)):
-                     with st.container():
-                            row = moviesn.iloc[i].values.tolist()
-                            imdbID = links.loc[links['movieId'] == row[0]].values.tolist()[0][1]
-                            imdbID = str(int(imdbID)).zfill(7)
-                            col1, col2 = st.columns([2,5])
 
-                            with col1:
-                                   st.image(htp5+imdbID, width=150)
-                            with col2:
-                                   st.write(f'''<h3>{row[2]}</h3>
-                                                 {row[3]}<br>
-                                                 {round(row[4],1)}
-                                   ''', unsafe_allow_html=True)
+if selected== 'New User':
+       st.write('## Predictions for a new user')
+       options = st.multiselect('Select Your Favourite ',categories,['Action', 'Adventure'])
+       # st.write('You selected:', options)
+       newButton = st.button('Predict_for_new_user')
+       user_vec = {'userId':0, 'userRatingCount':0, 'userAvgRating':0, 'Action':0, 'Adventure':0,
+                     'Animation':0, 'Children':0, 'Comedy':0, 'Crime':0, 'Documentary':0, 'Drama':0,
+                     'Fantasy':0, 'Film-Noir':0, 'Horror':0, 'IMAX':0, 'Musical':0, 'Mystery':0,
+                     'Romance':0, 'Sci-Fi':0, 'Thriller':0, 'War':0, 'Western':0}
+       htp5= 'https://img.omdbapi.com/?apikey=a50d9a01&i=tt'
 
+       if newButton:
+              with st.spinner('Wait for it...'):
+                     for i in options:
+                            user_vec[i] = 5
+                     moviesn = newUserPredict(user_vec)
+                     # st.write(moviesn)
+                     
+                     for i in range(len(moviesn)):
+                            with st.container():
+                                   row = moviesn.iloc[i].values.tolist()
+                                   imdbID = links.loc[links['movieId'] == row[0]].values.tolist()[0][1]
+                                   imdbID = str(int(imdbID)).zfill(7)
+                                   col1, col2 = st.columns([2,5])
+
+                                   with col1:
+                                          st.image(htp5+imdbID, width=150)
+                                   with col2:
+                                          st.write(f'''<h3>{row[2]}</h3>
+                                                        {row[3]}<br>
+                                                        {round(row[4],1)}
+                                          ''', unsafe_allow_html=True)
+                                          with st.expander(""):
+                                                 st.write("""
+                                                        The chart above shows some numbers I picked for you.
+                                                        I rolled actual dice for these, so they're *guaranteed* to
+                                                        be random.
+                                                 """)
+                                                 st.image("https://static.streamlit.io/examples/dice.jpg")
 
 
 
@@ -152,34 +171,33 @@ def displayUserRow(uid):
        st.table(rowe.style.format("{:.1f}"))
 
 
+if selected== 'Existing Users':
+       st.write('## Predictions for an existing user')
 
-st.write('<br><br>' , unsafe_allow_html=True)
-st.write('## Predictions for an existing user')
+       uid = 1
+       st.text_input('Enter User Id' ,uid ,key="placeholder")
+       uid = int(st.session_state.placeholder)
+       displayUserRow(uid)
 
-uid = 1
-st.text_input('Enter User Id' ,uid ,key="placeholder")
-uid = int(st.session_state.placeholder)
-displayUserRow(uid)
+       existingButton = st.button('Predict_for_existing_user')
+       if existingButton:
+              with st.spinner('Wait for it...'):
+                     moviese = existingUserPredict(uid)
+                     st.write(moviese)
+                     st.write('<br>' , unsafe_allow_html=True)
+              for i in range(len(moviese)):
+                            with st.container():
+                                   row = moviese.iloc[i].values.tolist()
+                                   imdbID = links.loc[links['movieId'] == row[0]].values.tolist()[0][1]
+                                   imdbID = str(int(imdbID)).zfill(7)
+                                   col1, col2 = st.columns([2,5])
 
-existingButton = st.button('Predict_for_existing_user')
-if existingButton:
-       with st.spinner('Wait for it...'):
-              moviese = existingUserPredict(uid)
-              st.write(moviese)
-              st.write('<br>' , unsafe_allow_html=True)
-       for i in range(len(moviese)):
-                     with st.container():
-                            row = moviese.iloc[i].values.tolist()
-                            imdbID = links.loc[links['movieId'] == row[0]].values.tolist()[0][1]
-                            imdbID = str(int(imdbID)).zfill(7)
-                            col1, col2 = st.columns([2,5])
-
-                            with col1:
-                                   st.image(htp5+imdbID, width=150)
-                            with col2:
-                                   st.write(f'''<h3>{row[3]}</h3>
-                                                 {row[4]}<br>
-                                                 {round(row[5],1)}
-                                   ''', unsafe_allow_html=True)
+                                   with col1:
+                                          st.image(htp5+imdbID, width=150)
+                                   with col2:
+                                          st.write(f'''<h3>{row[3]}</h3>
+                                                        {row[4]}<br>
+                                                        {round(row[5],1)}
+                                          ''', unsafe_allow_html=True)
 
 
